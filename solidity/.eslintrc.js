eafe54ecb1903412ed67df11c4ecafe0aa668545
Piotr <piotr.ostrowski@aol.com>
2022-01-25 18:19:43 +0100

    add solidity conf files

---
+++ solidity/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
